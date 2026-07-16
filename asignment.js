dc8d180d9c87750767eeed63688339b8939bcf3a
Hadis Amini <hadis.amini90@gmail.com>
2026-07-16 21:01:32 +0330

    Initial bookstore project

---
+++ asignment.js
@@ -0,0 +1,393 @@
+// Assignments JS
+
+// Values and Variables
+// Data Types
+
+/*let country = "Iran";
+console.log(typeof country);
+
+let continent = "Asia";
+console.log(continent);
+
+let population = 92;
+console.log(typeof population);
+
+let isIsland = true;
+console.log(typeof isIsland); */
+
+// let, const and var
+
+// language = 'persian';
+// const country = 'Iran';
+// const continent = 'Asia';
+// const isIsland = true;
+// isIsland = true;
+
+// LECTURE: Basic Operators
+
+// let iranPopulation = 92;
+// console.log(iranPopulation / 2);
+// iranPopulation++;
+// let finlandPopulation = 6;
+// console.log(iranPopulation > finlandPopulation);
+// let averageCountry = 33;
+// console.log(iranPopulation<averageCountryCountry);
+
+// LECTURE: Strings and Template Literals
+
+// const country = "Iran";
+// const continent = "Asia";
+// const population = 92;
+// const language = "persian";
+
+// const description = `${country}  is in ${continent}, and its ${population} million people speak ${language}`;
+// console.log(description);
+
+// LECTURE: Taking Decisions: if / else Statements
+
+// const population = 92;
+// const averagePopulation = 33;
+// country = "Iran";
+
+// if(population>33) {
+//   console.log(`${country}'s population is above average`);
+// } else{
+// const populationBelow = averagePopulation - population;
+// console.log(`${country}' population is ${populationBelow} million below average`)
+// };
+
+// LECTURE: Type Conversion and Coercion
+
+// '9' - '5'; // 4
+// '19' - '13' + '17'; // 6 + '17' = 617
+// '19' - '13' + 17; // 6 + 17 = 23
+// '123' < 57;  // false
+// 5 + 6 + '4' + 9 - 4 - 2; // 11 + '4' = '114' + 9 = 1149 - 4 - 2 = 1143
+
+// LECTURE: Equality Operators: == vs. ===
+
+// const numNeighbours = Number(prompt('How many neighbour countries does your country have?'));
+
+// if(numNeighbours === 1) {
+//   console.log('Only 1 border!');
+// } else if(numNeighbours > 1) {
+//   console.log('More than 1 border');
+// } else {
+// console.log('No borders');
+// };
+
+// LECTURE: Logical Operators
+
+// const language = 'persian';
+// const population = 92;
+// const isIsland = false;
+
+// if (language === 'english' && population < 50 && !isIsland) {
+// console.log('You should live in Iran :)');
+// } else{
+// console.log('Iran does not meet your criteria :(');
+// };
+
+// LECTURE: The switch Statement
+
+// const language = "chinese";
+// switch (language) {
+//   case "chinese":
+//   case "mandarin":
+//     console.log("MOST number of native speakers!");
+//     break;
+//   case "spanish":
+//     console.log("2nd place in number of native speakers");
+//     break;
+//   case "english":
+//     console.log("3rd place");
+//     break;
+//   case "hindi":
+//     console.log("Number 4");
+//     break;
+//   case "arabic":
+//     console.log("5th most spoken language");
+//     break;
+//   default:
+//     console.log("Great language too");
+// }
+
+// LECTURE: The Conditional (Ternary) Operator
+
+// const tip = 15/100; // 0.15
+// const tip = 20/100; // 0.2
+// console.log(tip);
+
+// const bill = 430;
+// const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// console.log(tip);
+
+// console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+
+// LECTURE: Taking Decisions: if / else Statements
+
+// const population = 92;
+// const description =
+//   population > 33
+//     ? `Iran's population is above average.`
+//     : `Iran's population is below average.`;
+// console.log(description);
+
+// LECTURE: Functions
+
+// function percentageOfWorld1(country, population) {
+// return `${country} has ${population} million people, so it's about ${(population / 7900) * 100} of the world population`;
+// }
+
+// const china = percentageOfWorld1 ('China', 1441);
+// console.log(china);
+
+// const iran = percentageOfWorld1 ('Iran', 92);
+// console.log(iran);
+
+// const japan = percentageOfWorld1 ('Japan', 123);
+// console.log(japan);
+
+// const percentageOfWorld2 = function (country, population) {
+// return `${country} has ${population} million people, so it's about ${(population / 7900) * 100} of the world population`;
+// }
+
+// LECTURE: Function Declarations vs. Expressions
+
+// const china = percentageOfWorld2('China', 1441);
+// console.log(china);
+
+// const iran = percentageOfWorld2('Iran', 92);
+// console.log(iran);
+
+// const japan = percentageOfWorld2('Japan', 123);
+// console.log(japan);
+
+// LECTURE: Arrow Functions
+
+// const percentageOfWorld3 = (country, population) => {
+//   const percentage = (population / 7900) * 100;
+
+//   return `${country} has ${population} million people, so it's about ${percentageا} of the world population`;
+// };
+
+// console.log(percentageOfWorld3("China", 1441));
+
+// LECTURE: Functions Calling Other Functions
+
+// function percentageOfWorld1(population) {
+//   return (population / 7900) * 100;
+// }
+// function describePopulation(country, population) {
+// const percentage = percentageOfWorld1(population);
+
+//   return `${country} has ${population} million people, which is about ${percentage} of the world.`
+// }
+//  const describePopulation1 = describePopulation('Iran', 92);
+//  console.log(describePopulation1);
+
+//  const describePopulation2 = describePopulation('Japan', 123);
+//  console.log(describePopulation2);
+
+//  const describePopulation3 = describePopulation('China', 1441);
+//  console.log(describePopulation3);
+
+// function percentageOfWorld1(population) {
+//   return (population / 7900) * 100;
+// }
+// const describePopulation = function (country, population) {
+//   const percentage = percentageOfWorld1(population);
+//   const description = `${country} has ${population} million
+// people, which is about ${percentage}% of the world.`;
+//   console.log(description);
+// };
+// describePopulation("Portugal", 10);
+// describePopulation("China", 1441);
+// describePopulation("USA", 332);
+
+// LECTURE: Introduction to Arrays
+
+// const populations = [92, 123, 52, 62];
+
+// console.log(populations);
+
+// function percentageOfWorld1(population) {
+//   return (population / 7900) * 100;
+// }
+// const populations = [92, 123, 52, 62];
+// const percentages = [
+//   percentageOfWorld1(populations[0]),
+//   percentageOfWorld1(populations[1]),
+//   percentageOfWorld1(populations[2]),
+//   percentageOfWorld1(populations[3]),
+// ];
+// console.log(percentages);
+
+//           مرور روز 6
+
+// // JavaScript
+
+// به جای اینکه فقط دفترت را بخوانی، این کار را بکن:
+
+// اول بدون نگاه کردن به دفتر، سعی کن بنویسی:
+// Arrow Function ***
+// Function Declaration ***
+// Function Expression  ***
+// if / else if / else ***
+// switch ***
+// Array ****
+// ⏳ Ternary Operator (? :) ***
+// ⏳ Function Calling Other Functions ****
+// ⏳ percentageOfWorld1 و describePopulation
+// ⏳ مرور Challengeها (لازم نیست همه را کامل بنویسی؛ یکی دو تا کافی است.)
+// اگر جایی یادت نبود، فقط همان قسمت را از روی دفتر مرور کن.
+// یکی دو تمرین قدیمی را دوباره حل کن.
+
+// این روش خیلی مؤثرتر از فقط خواندن یادداشت‌هاست.
+
+// Arrow Function
+
+// const retirement = (birthyear) => {      ارو فانکشن مساوی کوچکتر داره
+// return 2027 - birthyear ;
+// };
+
+// const age1 = retirement(1990);
+// console.log(age1);
+
+// Function Declaration
+
+// function age (birthYear) {
+//   return 2027 - birthYear;
+// }
+
+// const hadis = age (1990);
+// console.log(hadis);
+
+// // Arrow Function
+
+// const age = (birthYear) => {
+//   return 2027 - birthYear;
+// };
+
+// const hadis = age(1990);
+// console.log(hadis);
+
+// Function Expression
+
+// const age = function (birthYear) {
+//   return 2027 - birthYear;
+// };
+
+// const hadis = age(1990);
+// console.log(hadis);
+
+// function age(birthYear) {
+//   return 2027 - birthYear;
+// }
+
+// const hadis = age(1990);
+// console.log(hadis);
+
+// const age = function (birthYear) {
+//   return 2027 - birthYear;
+// };
+
+// const hadis = age(1990);
+// console.log(hadis);
+
+// const age = (birthYear) => {
+//   return 2027 - birthYear;
+// };
+
+// const hadis = age(1990);
+// console.log(hadis);
+
+// if / else if / else
+
+// const aveScore = (
+//   score1,
+//   score2,
+//   score3, //مساوی علامت نذاشتی
+// ) => (score1 + score2 + score3) / 3;
+
+// const checkWinner = (aveScoreDolphins, aveScoreSagha) => {
+//   //مساوی علامت نذاشتی
+//   if (aveScoreDolphins >= 2 * aveScoreSagha) {
+//     //      در جاوااسکریپت شرط باید داخل پرانتز باشد:
+//     console.log(
+//       `dolphins win the game (${aveScoreDolphins} vs. ${aveScoreSagha})`,
+//     );
+//   } else if (aveScoreSagha >= 2 * aveScoreDolphins) {
+//     // else if
+//     console.log(
+//       `Sagha win the game (${aveScoreSagha} vs. ${aveScoreDolphins})`,
+//     ); // در جواب شرط باید داخل کانسول لاگ باشه
+//   } else {
+//     console.log("no one win the game!");
+//   }
+// };
+
+// const aveScoreDolphins = aveScore(20, 10, 40); /// تابع صدا نزدی اواسکور
+// console.log(aveScoreDolphins);
+
+// const aveScoreSagha = aveScore(30, 80, 40);
+// console.log(aveScoreSagha);
+
+// checkWinner(aveScoreDolphins, aveScoreSagha);
+
+// -----------------------------
+
+// const aveScoreDolphis = (10, 15, 20);
+// console.log(aveScore);
+
+// switch
+
+// const day = "friday"
+// switch (day) {
+//   case "friday":
+//     console.log("rest day");
+//     break;
+
+//   case "saturday":
+//     console.log("workday");
+//     break;
+
+//   case "sunday":
+//     console.log("eat day");
+//     break;
+
+//   default:
+//   console.log("Invalid day");
+// }
+
+// // Array
+
+// const friends = ['Jack', 'Joy', 'lucy'];
+// console.log(friends[friends. length - 1]);
+
+// const age = 20;
+// const result = age >= 18 ? "Adult" : "Child";
+
+// console.log(result);
+
+// const age = 10;
+// const result = age >= 18 ? "you can dirnk wine" : " you can drink water";
+
+// console.log(result);
+
+// age = 85
+// const result = age >= 55 ? "sex" : "Jagh";
+// console.log(result);
+
+// function percentageOfWorld (population) {
+//   return (population / 7900) * 100;
+// }
+
+// const describePopulation = function (country, population) {
+//   const percentage = percentageOfWorld (population);
+//   const description = `${country} has ${population} million people, which is about ${percentage}% of the world.`;
+//   console.log(description);
+// };
+
+// describePopulation("iran", 92);
+// describePopulation("Japan", 132);
---
+++ script.js
@@ -0,0 +1,213 @@
+"use strict";
+
+/* let hasDriverLicense= false;
+const passTest = true;
+
+if (passTest) hasDriverLicenses = true;
+if (hasDriverLicense) console.log('I can  drive');
+
+// const interface = 'Audio';
+const private = 534; 
+
+function logger() {
+  console.log('My name is Hadis');
+}
+
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+
+function describeCountry(country, population, capitalCity) {
+  const description = `${country} has ${population} million people and its capital city is ${capitalCity}`;
+  return description;
+}
+
+const firstInformation = describeCountry('Finland', 6, 'Helsinki');
+console.log(firstInformation);
+
+const secondInformation = describeCountry('Iran', 92, 'Tehran');
+console.log(secondInformation);
+
+const thirdInformation = describeCountry('China', 150, 'Beijing');
+console.log(thirdInformation);
+
+
+
+const age1 = calcAge1(1991); 
+
+  function calcAge1(birthYear) {
+    return 2037 - birthYear;
+  }
+
+ const age2 = calAge2(1991);
+
+ const calAge2 = function (birthYear) {
+   return  2037 - birthYear;
+ }
+
+ console.log(age2);
+
+  const calAge2 = function (birthYear) {
+   return  2037 - birthYear;
+ }
+
+ Arrow function 
+  
+const calcAge3 =  birthYear => 2027 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+
+
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+
+  const juice = `Juice with ${applePieces} piece of apple and ${orangePieces} piece of orange.`;
+  return juice;
+};
+
+console.log(fruitProcessor(2, 3));
+
+
+
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+const avgDolphins = calcAverage(85, 54, 41);
+console.log(avgDolphins);
+
+const avgKoalas = calcAverage(23, 34, 27);
+console.log(avgKoalas);
+
+
+
+
+
+
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+const avgDolphins = calcAverage(44, 23, 71);
+console.log(avgDolphins);
+
+const avgKoalas = calcAverage(65, 54, 49);
+console.log(avgKoalas);
+
+function checkWinner(avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win (${avgDolphins} vs ${avgKoalas})`);
+  } else {
+    console.log(`Koalass win (${avgKoalas} vs ${avgDolphins})`);
+  }
+}
+ console.log(checkWinner(avgDolphins, avgKoalas));
+
+
+
+
+
+
+
+
+
+
+const calcAverage = (score1, score2, score3) =>
+  (score1 + score2 + score3) / 3;
+
+function checkWinner(avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+  } else {
+    console.log('No team wins!');
+  }
+}
+
+// Data 1
+let avgDolphins = calcAverage(44, 23, 71);
+let avgKoalas = calcAverage(65, 54, 49);
+
+console.log(avgDolphins);
+console.log(avgKoalas);
+
+checkWinner(avgDolphins, avgKoalas);
+
+// Data 2
+avgDolphins = calcAverage(85, 54, 41);
+avgKoalas = calcAverage(23, 34, 27);
+
+console.log(avgDolphins);
+console.log(avgKoalas);
+
+checkWinner(avgDolphins, avgKoalas);
+
+
+const friend1 = 'Micheal';
+const friend2 = 'Steven';
+const friend3 = 'Peter';
+
+const friends = ['Micheal', 'Steveen', 'Peter'];
+console.log(friends);
+
+const years = new Array (1990, 1984, 2008, 2027);
+console.log(years);
+
+console.log(friends[2]);
+console.log(friends[0]);
+
+console.log(friends.length);
+console.log(friends[friends.length-1 ]);
+
+
+const firstName = 'Hadis';
+const hadis = ['Hadis', 'Amini', 2027-1990, 'programmer', friends];
+console.log(hadis);
+
+Exercise 
+const calcAge = function  (birthyear) {
+  return 2037 - birthyear;
+}
+
+const years = [1990, 1967, 2002, 2010, 2018];
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length-1]);
+console.log(age1, age2, age3);
+
+
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length-1])];
+console.log(ages);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
