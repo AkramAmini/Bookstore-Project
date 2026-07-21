a30894a18d34798535135b952dbeef35b68c47ff
Hadis Amini <hadis.amini90@gmail.com>
2026-07-21 17:21:38 +0330

    Add developer skills section 3

--- asignment.js
+++ asignment.js
@@ -691,7 +691,6 @@ console.log(typeof isIsland); */
 // // console.log(tips);
 // // console.log(totals);
 
-
 // function calcTip(bill) {
 //   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 // }
@@ -708,7 +707,6 @@ console.log(typeof isIsland); */
 // console.log(tips);
 // console.log(totals);
 
-
 // // Bonus
 // function calcAverage(arr) {
 //   let sum = 0;
@@ -720,4 +718,46 @@ console.log(typeof isIsland); */
 //  return sum / arr.length;
 // }
 
-// console.log(calcAverage(totals)); 
+// console.log(calcAverage(totals));
+
+//////////////////////////////// Problem 1 ////////////////////////////////
+
+// const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+// const calcTempAmplitude = function (temps) {
+//   let max = temps[0];
+//   let min = temps[0];
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== 'number') continue;
+
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+//   console.log(max, min);
+//   return max - min;
+// };
+// const amplitude = calcTempAmplitude(temperatures);
+// console.log(amplitude);
+
+// //////////////////////////////// Problem 2 ////////////////////////////////
+
+// const calcTempAmplitudeNew = function (t1, t2) {
+//   const temps = t1.concat(t2);
+//   console.log(temps);
+
+//   let max = temps[0];
+//   let min = temps[0];
+
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== 'number') continue;
+
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+//   console.log(max, min);
+//   return max - min;
+// };
+// const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
+// console.log(amplitudeNew);
--- script.js
+++ script.js
@@ -1,7 +1,56 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const x = '23';
+// const x = '23';
 
-const calcAge = birthYear => 2037 - birthYear;
-console.log(calcAge(1991));
+// const calcAge = birthYear => 2037 - birthYear;
+// console.log(calcAge(1991));
+
+// const measureKelvin = function () {
+//   const measurement = {
+//     tyep: 'temp',
+//     unit: 'celsius',
+//     value: prompt('Degrees celsius:'),
+//   };
+
+//   // console.log(measurement);
+//   console.table(measurement);
+  
+
+// // console.log(measurement.value);
+// // console.warn(measurement.value);
+// // console.error(measurement.value);
+
+
+
+
+
+//   // const kelvin = +prompt(measurement.value) + 273;
+//   return kelvin;
+// };
+
+// console.log(measureKelvin());
+
+// using a debugging 
+
+
+// const calcTempAmplitudeNewBug = function (t1, t2) {
+//   const temps = t1.concat(t2);
+//   console.log(temps);
+
+//   let max = 0;
+//   let min = 0;
+
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== 'number') continue;
+
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+//   console.log(max, min);
+//   return max - min;
+// };
+// const amplitudeNew = calcTempAmplitudeNewBug([3, 5, 1], [9, 4, 5]);
+// console.log(amplitudeNew);
+
