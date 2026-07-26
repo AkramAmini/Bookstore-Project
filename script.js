413447b5ddac4de6989ccd8c569910b0d58bf2e2
Hadis Amini <hadis.amini90@gmail.com>
2026-07-26 17:50:03 +0330

    Implement Pig Game logic

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
 'use strict';
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let currentScore = 0;
 
 
-const openModal = function () {
-console.log('Button clicked');
-modal.classList.remove('hidden');
-overlay.classList.remove('hidden');
-  };
+btnRoll.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+  
+diceEl.classList.remove('hidden');
+diceEl.src = `dice-${dice}.png`;
+
+if(dice !== 1) {
+currentScore += dice;
+current0El.textContent = currentScore;
+} else {
+
+}
+})
 
 
- const closeModal = function() {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-  };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
-
-  btnCloseModal.addEventListener('click', closeModal);
-  overlay.addEventListener('click', closeModal);
-   
-  document.addEventListener('keydown', function (e) {
-    console.log(e.key);
-    if(e.key === 'Escape' && !modal.classList.contains('hidden')) {
-        closeModal();
-    }
-  });
