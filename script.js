d4126582c545da113dcdde6d7aca13774773a124
Hadis Amini <hadis.amini90@gmail.com>
2026-07-30 15:14:18 +0330

    Hoisting And Tdz In Practice

--- script.js
+++ script.js
@@ -1,38 +1,87 @@
 'use strict';
 
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
 
-function printAge() {
-  const output = `${firstName}, you are ${age}, born in ${birthYear}`
-  console.log(output);
+// function printAge() {
+//   const output = `${firstName}, you are ${age}, born in ${birthYear}`
+//   console.log(output);
 
-  if(birthYear >= 1981 && birthYear<= 1996) {
-    var millenial = true;
-    const firstName = 'Steven';
+//   if(birthYear >= 1981 && birthYear<= 1996) {
+//     var millenial = true;
+//     const firstName = 'Steven';
 
-     output = 'NEW OUTPUT!';
+//      output = 'NEW OUTPUT!';
 
-    const str = `Oh, and you're a millenial, ${firstName}`;
-    console.log(str);
+//     const str = `Oh, and you're a millenial, ${firstName}`;
+//     console.log(str);
 
-    function addd(a, b) {
-      return a + b;
-    }
+//     function addd(a, b) {
+//       return a + b;
+//     }
    
-  }
-  // console.log(str);
-  console.log(millenial);
-  // console.log(addd(2, 3));
-  console.log(output);
+//   }
+//   // console.log(str);
+//   console.log(millenial);
+//   // console.log(addd(2, 3));
+//   console.log(output);
   
-}
-printAge();
-
-  return age;
-}
-
-const firstName = 'Jonas';
-calcAge(1991);
-// console.log(age);
+// }
 // printAge();
+
+//   return age;
+// }
+
+// const firstName = 'Jonas';
+// calcAge(1991);
+// // console.log(age);
+// // printAge();
+
+
+
+
+
+
+
+// console.log(me);
+// console.log(job);
+// console.log(year);
+
+// var me = 'Jonas';
+// let job = 'teacher';
+// const year = 1991;
+
+// console.log(addDecl(2, 3));
+// console.log(addExper(2, 3));
+// console.log(addArrow(2, 3));
+
+
+function addDecl(a,b) {
+  return a + b;
+}
+
+var addExper = function (a,b) {
+  return a + b;
+}
+
+var addArrow = (a, b)=>a+b;
+
+console.log(undefined);
+
+if(!numProducts) deletShoppingCart();
+
+var numProducts = 10;
+
+function deletShoppingCart() {
+  console.log('All product deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
+
+
