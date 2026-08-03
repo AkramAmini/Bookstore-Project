7bd29592387abd6c29e8d20cedd9b611c153f565
Hadis Amini <hadis.amini90@gmail.com>
2026-08-04 01:50:47 +0330

    Solve JavaScript data structures and modern operators challenge

--- script.js
+++ script.js
@@ -1,186 +1,95 @@
 'use strict';
 
-// // Data needed for a later exercise
-// const flights =
-//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
-// // Data needed for first part of the section
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-
-  order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
-
-  orderDelivery: function ({
-    starterIndex = 1,
-    mainIndex = 0,
-    time = '20:00',
-    address,
-  }) {
-    console.log(
-      `order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`,
-    );
-  },
-
-  orderPasta: function (ing1, ing2, ing3) {
-    console.log(
-      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`,
-    );
-  },
-
-  orderPizza: function (mainIngredient, ...otherIngredients) {
-    console.log(mainIngredient);
-    console.log(otherIngredients);
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
   },
 };
 
-const rest1 = {
-  name: 'Capri',
-  // numGuests:20,
-  numGuests: 0,
-};
+// // برای هر تیم یک آرایه از بازیکنان ایجاد کن و آن‌ها را در دو متغیر به نام‌های players1 و players2 ذخیره کن.
+// const [players1, players2] = game.players;
+// console.log(game.players);
 
-const rest2 = {
-  name: 'La Piazza',
-  owner: 'Giovanni Rossi',
-};
+// //دروازه‌بان تیم اول را در متغیر gk ذخیره کن و بقیه ۱۰ بازیکن را در آرایه fieldPlayers قرار بده.
 
-// OR assigment operator
-// rest1.numGuests = rest1.numGuests || 10;
-// rest2.numGuests = rest2.numGuests || 10;
-// rest1.numGuests ||= 10:
-// rest2.numGuests ||= 10;
+// const [gk, ...fieldPlayers] = players1;
+// console.log(gk);
+// console.log(fieldPlayers);
 
+// //یک آرایه به نام allPlayers بساز که شامل بازیکنان هر دو تیم (۲۲ بازیکن) باشد
 
-// nullish assigmnet operator (null or undefined)
-// rest1.numGuests ??= 10;
-// rest2.numGuests ??= 10;
+// const allPlayers = [...players1, ...players2];
+// console.log(allPlayers);
 
-//AND assigment operator
-// rest1.owner = rest2.owner && '<ANONYMOUS>';
-// rest2.owner = rest2.owner && '<ANONYMOUS>';
-rest1.owner &&= '<ANONYMOUS>';
-rest2.owner &&= '<ANONYMOUS>';
+// //یک آرایه جدید به نام players1Final بساز که شامل تمام بازیکنان اصلی تیم اول (players1) به اضافه سه بازیکن ذخیره باشد: Thiago، Coutinho و Perisic.
 
+// const players1Final = [...players1,  'Thiago', 'Coutinho', 'Perisic' ];
+// console.log(players1Final);
 
-console.log(rest1.owner);
-console.log(rest2.owner);
+// //از آبجکت game.odds سه متغیر بساز:
+// // team1 → ضریب برد تیم اول
+// // draw → ضریب مساوی
+// // team2 → ضریب برد تیم دوم
 
+// const { team1, x: draw, team2 } = game.odds;
+// console.log(team1);
+// console.log(draw);
+// console.log(team2);
 
+// //یک تابع به نام printGoals بساز که هر تعداد نام بازیکن دریافت کند و نام هر بازیکن را در کنسول چاپ کند. همچنین تعداد کل گل‌ها را (برابر با تعداد بازیکن‌های واردشده) چاپ کند.
 
+// const printGoals = function (...players) {
+//   for (const player of players) {
+//     console.log(player);
+//   }
 
-
-
-
-
-
-
-
-// restaurant.numGuests = 0;
-// const guests = restaurant.numGuests || 10
-// console.log(guests);
-
-// // Nullish: null and undefined (NOT 0 or '')
-// const guestCorrect = restaurant.numGuests ?? 10;
-// console.log(guestCorrect);
-
-
-
-
-// console.log('----- OR -----');
-
-// // Use ANY data type, return ANY data type, short-circuiting 
-// console.log(3 || 'Jonas');
-// console.log('' || 'Jonas');
-// console.log(true || 0);
-// console.log(undefined || null);
-
-// console.log(undefined || 0 || 'Hello' || 23 || null);
-
-// restaurant.numGuests = 0;
-// const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
-// console.log(guests1);
-
-// const guests2 = restaurant.numGuests || 10
-// console.log(guests2);
-
-
-// console.log('----- AND -----');
-// console.log(0 && 'Jonas');
-// console.log(7 && 'Jonas');
-
-// console.log('Hello' && 23 && null && 'Jonas');
-
-// // Practice example
-// if(restaurant.orderPizza) {
-//   restaurant.orderPizza('mushrooms', 'spinach');
-// }
-
-// restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
-
-
-
-
-
-
-
-
-// // 1) Destructuring
-
-// // SPREAD, becouse on RIGHT side of
-// const arr = [1, 2, ...[3, 4]];
-// console.log(arr);
-
-
-// // REST, because on left side of =
-// const [a, b, ...others] = [1, 2, 3, 4, 5];
-// console.log(a, b, others);
-
-// const [pizza, , risotto, ...otherFood] = [
-//   ...restaurant.mainMenu,
-//   ...restaurant.starterMenu,
-// ];
-// console.log(pizza, risotto, otherFood);
-
-// //Object
-// const { sat, ...weekdays } = restaurant.openingHours;
-// console.log(weekdays);
-
-// // 2) Functions
-
-// const add = function (...numbers) {
-//   console.log(numbers);
-//   let sum = 0;
-//   for (let i = 0; i < numbers.length; i++) sum += numbers[i];
-//   console.log(sum);
+//   console.log(`${players.length} goals were scored`);
 // };
 
-// add(2, 3);
-// add(5, 3, 7, 2);
-// add(8, 2, 5, 3, 2, 1, 4);
+// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+// printGoals(...game.scored);
 
-// const x = [23, 5, 7];
-// add(...x);
+//تیمی که ضریب برد کمتری دارد را پیدا کن و نام آن تیم را در کنسول چاپ کن؛ بدون استفاده از if/else و بدون استفاده از عملگر سه‌تایی (? :).
+// یعنی:
 
-// restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
-// restaurant.orderPizza('mushrooms');
+// اگر team1 ضریب کمتری داشت → نام تیم اول را چاپ کن.
+// اگر team2 ضریب کمتری داشت → نام تیم دوم را چاپ کن.
+const team1 = game.odds.team1;
+const team2 = game.odds.team2;
+
+console.log((team1 < team2 && game.team1) || game.team2);
