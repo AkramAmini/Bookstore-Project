622c46252774311b07b8077b83d0f8a89a20955d
Hadis Amini <hadis.amini90@gmail.com>
2026-08-03 13:06:09 +0330

    Nullish Coalescing

--- script.js
+++ script.js
@@ -54,37 +54,47 @@ const restaurant = {
   },
 };
 
-
-console.log('----- OR -----');
-
-// Use ANY data type, return ANY data type, short-circuiting 
-console.log(3 || 'Jonas');
-console.log('' || 'Jonas');
-console.log(true || 0);
-console.log(undefined || null);
-
-console.log(undefined || 0 || 'Hello' || 23 || null);
-
 restaurant.numGuests = 0;
-const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
-console.log(guests1);
+const guests = restaurant.numGuests || 10
+console.log(guests);
 
-const guests2 = restaurant.numGuests || 10
-console.log(guests2);
+// Nullish: null and undefined (NOT 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
 
 
-console.log('----- AND -----');
-console.log(0 && 'Jonas');
-console.log(7 && 'Jonas');
 
-console.log('Hello' && 23 && null && 'Jonas');
 
-// Practice example
-if(restaurant.orderPizza) {
-  restaurant.orderPizza('mushrooms', 'spinach');
-}
+// console.log('----- OR -----');
 
-restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+// // Use ANY data type, return ANY data type, short-circuiting 
+// console.log(3 || 'Jonas');
+// console.log('' || 'Jonas');
+// console.log(true || 0);
+// console.log(undefined || null);
+
+// console.log(undefined || 0 || 'Hello' || 23 || null);
+
+// restaurant.numGuests = 0;
+// const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+// console.log(guests1);
+
+// const guests2 = restaurant.numGuests || 10
+// console.log(guests2);
+
+
+// console.log('----- AND -----');
+// console.log(0 && 'Jonas');
+// console.log(7 && 'Jonas');
+
+// console.log('Hello' && 23 && null && 'Jonas');
+
+// // Practice example
+// if(restaurant.orderPizza) {
+//   restaurant.orderPizza('mushrooms', 'spinach');
+// }
+
+// restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
 
 
 
