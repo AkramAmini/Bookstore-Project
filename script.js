2de08d6df29569d0985a58620cc15b59ebbcb795
Hadis Amini <hadis.amini90@gmail.com>
2026-08-04 19:48:54 +0330

    Learn optional chaining and object iteration

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const openingHours = {
     open: 11,
     close: 23,
   },
-  [weekdays[4]]: {
+  [weekdays[5]]: {
     open: 0, // Open 24 hours
     close: 12 + 12,
   },
@@ -54,24 +54,28 @@ const restaurant = {
   },
 };
 
-if (restaurant.openingHours && restaurant.openingHours.mon)
-  console.log(restaurant.openingHours.mon.open);
+// Property NAMES
+const properties = Object.keys(openingHours);
+console.log(properties);
 
-// WITH optional changing
-console.log(restaurant.openingHours.mon?.open);
-console.log(restaurant.openingHours?.mon?.open);
-
-// Example
-const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
-
-for (const day of days) {
-  const open = restaurant.openingHours[day]?.open ?? 'closed';
-  console.log(`on ${day}, we open at ${open}`);
+let openSr = `we are open on ${properties.length} days: `;
+for (const day of properties) {
+  openSr += `${day},`;
 }
 
-// Methods
-console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+console.log(openSr);
 
-// Arrays
-const users = [{ name: 'Jonas', email: 'hello@jonas.io' }];
-console.log(users[0]?.name ?? 'User array empty');
+// Property VALUES
+const values = Object.values(openingHours);
+console.log(values);
+
+// Entire object 
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+// [key, value]
+for(const [day, { open, close }] of entries) {
+  console.log(`On ${day} we open at ${open} and close at ${close}`);
+}
+
+
