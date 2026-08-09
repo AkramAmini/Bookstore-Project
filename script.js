256bb9a6f16af7e518ebf69219682e1d8a094c3c
Hadis Amini <hadis.amini90@gmail.com>
2026-08-09 08:32:40 +0330

    Learn primitives vs objects in functions

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
 'use strict';
 
-const bookings = [];
-
-const createBooking = function (
-  flightNum,
-  numPassengers = 1,
-  price = 199 * numPassengers,
-) {
-  //ES5
-  // numPassengers = numPassengers || 1;
-  // price = price || 199;
-
-  const booking = {
-    flightNum,
-    numPassengers,
-    price,
-  };
-  console.log(booking);
-  bookings.push(booking);
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 24739479284,
 };
 
-createBooking('LH123');
-createBooking('LH123', 2, 800);
-createBooking('LH123', 2);
-createBooking('LH123', 5);
+const CheckIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
 
-createBooking('LH123', undefined, 1000);
+  if (passenger.passport === 24739479284) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+CheckIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
+
+// Is the same as doing...
+const flightNum = flight;
+const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000000);
+};
+
+newPassport(jonas);
+CheckIn(flight, jonas);
+
+// اگر آبجکت به فانکشن ها بدهیم و آن ها را دستکاری کنیم آبجکت اصلی هم دستکاری میشه در تغییر دوم رانگ پسورد گرفتیم
