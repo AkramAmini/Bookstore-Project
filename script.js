92335b59b326ae3bedf709b58883cc6266602fc5
Hadis Amini <hadis.amini90@gmail.com>
2026-08-29 15:57:44 +0330

    Chapter 12 Lesson 5 - Use Remainder Operator to Style Rows

--- script.js
+++ script.js
@@ -298,3 +298,10 @@ btnSort.addEventListener('click', function (e) {
 //     console.log(i);
 //   }
 // }
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+    if (i % 2 === 0) row.style.backgroundColor = `orangered`;
+    if (i % 3 === 0) row.style.backgroundColor = `blue`;
+  });
+});
