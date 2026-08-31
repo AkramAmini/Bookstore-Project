f91255d7bf91be32379afb4708dea16be475a4cf
Hadis Amini <hadis.amini90@gmail.com>
2026-08-31 10:38:11 +0330

    Chapter 12 Lesson 11 - Internationalizing Dates (Intl)

--- script.js
+++ script.js
@@ -41,9 +41,9 @@ const account2 = {
     '2019-12-25T06:04:23.907Z',
     '2020-01-25T14:18:46.235Z',
     '2020-02-05T16:33:06.386Z',
-    '2020-04-10T14:43:26.374Z',
-    '2020-06-25T18:49:59.371Z',
-    '2020-07-26T12:01:20.894Z',
+    '2020-07-26T14:43:26.374Z',
+    '2020-07-11T18:49:59.371Z',
+    '2020-08-01T12:01:20.894Z',
   ],
   currency: 'USD',
   locale: 'en-US',
@@ -81,6 +81,22 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // Functions
 
+const formatMovementDate = function (date) {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+  console.log(daysPassed);
+
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+  const day = `${date.getDate()}`.padStart(2, 0);
+  const month = `${date.getMonth() + 1}`.padStart(2, 0);
+  const year = date.getFullYear();
+  return `${day}/${month}/${year}`;
+};
+
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
 
@@ -92,10 +108,7 @@ const displayMovements = function (acc, sort = false) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const date = new Date(acc.movementsDates[i]);
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0);
-    const year = date.getFullYear();
-    const displayDate = `${day}/${month}/${year}`;
+    const displayDate = formatMovementDate(date);
 
     const html = `
       <div class="movements__row">
@@ -447,8 +460,6 @@ btnSort.addEventListener('click', function (e) {
 
 // console.log(date2 - date1);
 
-
-
 // const diff = date2 - date1;
 
 // const days = diff / (1000 * 60 * 60 * 24);
@@ -462,7 +473,6 @@ btnSort.addEventListener('click', function (e) {
 
 // console.log(date1 < date2);
 // // true
-
 
 // const date = new Date(2026, 7, 20);
 
@@ -546,7 +556,6 @@ btnSort.addEventListener('click', function (e) {
 
 // console.log(date1);
 
-
 // // مقایسه تاریخ‌ها
 // date1 < date2;
 // date1 > date2;
@@ -583,3 +592,149 @@ btnSort.addEventListener('click', function (e) {
 
 // // Timestamp
 // date.getTime();
+
+// const date = new Date();
+
+// const dateFormatter = new Intl.DateTimeFormat('en-US');
+
+// console.log(dateFormatter.format(date));
+
+// const dateFormatter = new Intl.DateTimeFormat('fa-IR');
+
+// console.log(dateFormatter.format(date));
+
+// new Intl.DateTimeFormat('en-US').format(date);
+
+// new Intl.DateTimeFormat('en-GB').format(date);
+
+
+// const date = new Date(2026, 7, 31);
+
+// const formatter = new Intl.DateTimeFormat('en-US');
+
+// console.log(formatter.format(date));
+
+// const date = new Date(2026, 7, 31);
+
+// const formatter = new Intl.DateTimeFormat('fa-IR');
+
+// console.log(formatter.format(date));
+
+// const date = new Date(2026, 7, 31);
+
+// const formatter = new Intl.DateTimeFormat('de-DE');
+
+// console.log(formatter.format(date));
+
+// const options = {
+//   day: 'numeric',
+//   month: 'long',
+//   year: 'numeric',
+// };
+
+// const formatter = new Intl.DateTimeFormat('en-US', options);
+
+// console.log(formatter.format(date));
+
+// const options = {
+//   day: '2-digit',
+//   month: '2-digit',
+//   year: 'numeric',
+// };
+
+// const options = {
+//   hour: 'numeric',
+//   minute: 'numeric',
+// };
+
+// const formatter = new Intl.DateTimeFormat('en-US', options);
+
+// const options = {
+//   hour: 'numeric',
+//   minute: 'numeric',
+//   day: 'numeric',
+//   month: 'long',
+//   year: 'numeric',
+// };
+
+// const date = new Date(2026, 7, 5);
+
+// const options = {
+//   day: '2-digit',
+//   month: '2-digit',
+//   year: 'numeric',
+// };
+
+// const formatter = new Intl.DateTimeFormat('en-GB', options);
+
+// console.log(formatter.format(date));
+
+// const date = new Date(2026, 7, 5);
+
+// const options = {
+//   day: 'numeric',
+//   month: 'short',
+//   year: 'numeric',
+// };
+
+// const formatter = new Intl.DateTimeFormat('en-US', options);
+
+// console.log(formatter.format(date));
+
+// const options = {
+//   day: 'numeric',
+//   month: 'long',
+//   year: 'numeric',
+// };
+
+// const formatter = new Intl.DateTimeFormat('fa-IR', options);
+
+// console.log(formatter.format(new Date(2026, 7, 5)));
+
+
+
+// const options = {
+//   day: 'numeric',
+//   month: 'numeric',
+//   year: 'numeric',
+// };
+
+// const formatter = new Intl.DateTimeFormat(
+//   acc.locale,
+//   options
+// );
+
+// acc.locale = 'en-US';
+
+// const options = {
+//   day: 'numeric',
+//   month: 'numeric',
+//   year: 'numeric',
+// };
+
+// acc.locale = 'en-GB';
+
+// const date = new Date(2026, 0, 15);
+
+// const options = {
+//   day: '2-digit',
+//   month: 'short',
+//   year: 'numeric',
+// };
+
+// const formatter = new Intl.DateTimeFormat('en-US', options);
+
+// console.log(formatter.format(date));
+
+// const date = new Date(2026, 7, 15);
+
+// const options = {
+//   day: 'numeric',
+//   month: 'long',
+//   year: 'numeric',
+// };
+
+// const formatter = new Intl.DateTimeFormat('en-US', options);
+
+// console.log(formatter.format(date));
+
