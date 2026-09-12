7df070457fe6b06b361ead4c35ab7a6c33a1d1c3
Hadis Amini <hadis.amini90@gmail.com>
2026-09-12 23:10:23 +0330

    Chapter 13 Lesson 16 - Implement Sticky Navigation

--- script.js
+++ script.js
@@ -808,3 +808,24 @@ window.addEventListener('beforeunload', function (e) {
 //     navs.classList.remove('sticky');
 //   }
 // });
+
+// Sticky navigation: Intersection Observer API
+
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  // console.log(entry);
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
